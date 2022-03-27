381a823d8f31536712ab9f8f6592e6acb6b794b7
beiff <abeiphong@gmail.com>
2022-03-27 10:02:43 +0300

    сделал регистрацию через model c thymeleaf

--- src/main/java/com/example/demo/api/LoginController.java
+++ src/main/java/com/example/demo/api/LoginController.java
@@ -3,30 +3,21 @@ package com.example.demo.api;
 import com.example.demo.domain.User;
 import com.example.demo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.web.servlet.server.Session;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
-
-import javax.swing.text.html.HTML;
-import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-public class UserController {
-
+public class LoginController {
     private final UserService userService;
 
     @Autowired
-    public UserController(UserService userService) {
+    public LoginController(UserService userService) {
         this.userService = userService;
     }
-
-
-//    @RequestMapping(method = RequestMethod.GET)
-//    public String show(){
-//        return "index";
-//    }
 
     @PostMapping("/login")
     public String login(@RequestParam String email, @RequestParam String password)
@@ -46,19 +37,16 @@ public class UserController {
 
 
     @PostMapping("/register")
-    public String registerr(@RequestParam String username, @RequestParam String email, @RequestParam String password)
+    public String register(@ModelAttribute("user") User user)
     {
-        User user = new User();
         user.setRole("user");
-        user.setUsername(username);
-        user.setEmail(email);
-        user.setPassword(password);
         userService.regiserNewUser(user);
         return "redirect:login";
     }
 
     @GetMapping("/register")
-    public String show(){
+    public String registrationGet(Model model){
+        model.addAttribute("user", new User());
         return "registration";
     }
 }
